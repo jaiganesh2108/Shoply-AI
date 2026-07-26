6bbd8233a29432847099acfead7415d49618294e
jaiganesh2108 <jaig7335@gmail.com>
2026-07-26 21:20:05 +0530

    feat: add urls for refresh

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 
     #jwt token authentication urls
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
