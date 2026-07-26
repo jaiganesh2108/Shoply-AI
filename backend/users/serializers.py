db1c3550cc4aaf998c868712a1ab9c0fb9dd8e1b
jaiganesh2108 <jaig7335@gmail.com>
2026-07-26 20:29:01 +0530

    feat: implement the email field

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -11,5 +11,6 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
+            email=validated_data['email'],
             
         )
