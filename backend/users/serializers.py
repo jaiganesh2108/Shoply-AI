3fa9fd638d530fe322decc077d2e964a40cd10d7
jaiganesh2108 <jaig7335@gmail.com>
2026-07-26 20:27:06 +0530

    feat: user fields validation data

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -7,3 +7,8 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            
+        )
