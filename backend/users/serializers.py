65536d719ffbbe0a623e88f0ca7ea3240bbd2fd3
jaiganesh2108 <jaig7335@gmail.com>
2026-07-26 20:22:08 +0530

    feat: password field view

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,2 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    
