3ef07219b4ebef516f423b500018a585a1e128dd
jaiganesh2108 <jaig7335@gmail.com>
2026-07-26 21:19:18 +0530

    feat: add url to login

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,4 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("register/", RegisterView.as_view()),
+
+    #jwt token authentication urls
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
