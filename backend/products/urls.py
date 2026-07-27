de179c006337847b2d31ee927ca91e45c657d6f3
jaiganesh2108 <jaig7335@gmail.com>
2026-07-27 15:20:19 +0530

    feat: add path of product details

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,4 +3,5 @@ from .views import ProductListCreateView, ProductDetailView
 
 urlpatterns = [
     path("", ProductListCreateView.as_view(), name="product-list"),
+    path("<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
 ]
