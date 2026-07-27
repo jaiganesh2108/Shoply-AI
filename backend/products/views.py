90a7665bbba90b9d087b820375038fe734ff55b2
jaiganesh2108 <jaig7335@gmail.com>
2026-07-27 15:03:12 +0530

    feat: create the product list create view

--- backend/products/views.py
+++ backend/products/views.py
@@ -5,3 +5,5 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
