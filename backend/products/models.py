df9d902a76d14d649fec2ceb4096aeb521822e0b
jaiganesh2108 <jaig7335@gmail.com>
2026-07-27 14:35:57 +0530

    feat: changing the charfield

--- backend/products/models.py
+++ backend/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
