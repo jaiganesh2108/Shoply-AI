25d3d922c1c3c5badf5e99b1f0687a04c487b5b5
jaiganesh2108 <jaig7335@gmail.com>
2026-07-27 10:40:13 +0530

    feat: permission view

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import RegisterSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import api_view, permission_classes
 # Create your views here.
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
