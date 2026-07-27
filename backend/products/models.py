bf3e64b090755683cbe005195c5b610a31647584
jaiganesh2108 <jaig7335@gmail.com>
2026-07-27 14:48:07 +0530

    feat: migration update

--- backend/products/models.py
+++ backend/products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-    image = models.UrlField(blank=True)
+    image = models.URLField(blank=True)
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
