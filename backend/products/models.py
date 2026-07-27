e0199bd27cbdc60cbd1255605a0061bf096ba7df
jaiganesh2108 <jaig7335@gmail.com>
2026-07-27 14:28:11 +0530

    feat: positive integer field

--- backend/products/models.py
+++ backend/products/models.py
@@ -5,3 +5,4 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
