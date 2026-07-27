806eafde36e010f04a4832e246217ade45980b5e
jaiganesh2108 <jaig7335@gmail.com>
2026-07-27 10:47:10 +0530

    feat: views in users improved

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
 from .serializers import RegisterSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
