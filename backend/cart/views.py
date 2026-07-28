f2054572714581cd0fdfa5d962c286e7107ece25
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 14:25:58 +0530

    feat: import serializer from cart view

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Cart
+from .serializers import CartSerializer
