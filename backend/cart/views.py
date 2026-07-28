d96f6fabeb83647d57fb2145663ed98b43aa26b4
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 14:28:51 +0530

    feat: create the get_queryset function

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -5,3 +5,10 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Cart
 from .serializers import CartSerializer
+
+class CartListCreateView(generics.ListCreateAPIView):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
