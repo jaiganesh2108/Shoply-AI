fa58760e4bec8641351cecc6f426492ce746cca4
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 14:37:12 +0530

    feat: insert the query in the cart

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -15,3 +15,10 @@ class CartListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class CartDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
