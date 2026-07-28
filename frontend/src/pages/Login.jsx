71363d18da06602727e87dcb8ea5356eff4b5fda
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 20:28:21 +0530

    feat: adding the register user feature

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,57 @@
+import { useState } from "react";
+import { loginUser } from "../api/auth";
+
 function Login() {
-  return <h1>Login Page</h1>;
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const data = await loginUser({
+                username,
+                password,
+            });
+
+            localStorage.setItem("access", data.access);
+            localStorage.setItem("refresh", data.refresh);
+
+            alert("Login Successful!");
+        } catch (error) {
+            alert("Invalid Username or Password");
+        }
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <form onSubmit={handleLogin}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <br /><br />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <br /><br />
+
+                <button type="submit">
+                    Login
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default Login;
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+
 function Profile() {
-  return <h1>Profile Page</h1>;
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+
+        api.get("profile/")
+            .then((response) => {
+                setUser(response.data);
+            })
+            .catch(() => {
+                console.log("Not Logged In");
+            });
+
+    }, []);
+
+    if (!user)
+        return <h2>Loading...</h2>;
+
+    return (
+        <div>
+            <h1>Profile</h1>
+
+            <p>Username : {user.username}</p>
+
+            <p>Email : {user.email}</p>
+        </div>
+    );
 }
 
 export default Profile;
