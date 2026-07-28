b68536ea12fab5a9fd2fb83ccb23e51a02385928
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 22:25:54 +0530

    feat: appjs updations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,20 +7,21 @@ import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import Orders from "./pages/Orders";
 import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
     <Navbar />
-    
+
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/products" element={<Products />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/orders" element={<Orders />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/cart" element={ <ProtectedRoute> <Cart /> </ProtectedRoute> }/>
+      <Route path="/orders" element={ <ProtectedRoute> <Orders /> </ProtectedRoute> }/>
+      <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>}/>
     </Routes>
     </>
   );
