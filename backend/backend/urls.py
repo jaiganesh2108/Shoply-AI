924f43735af06ca8f8c1e0b5a60802d9a265fb9d
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 14:44:02 +0530

    feat: import the urls in backend urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/products/', include('products.urls')),
+    path("api/cart/", include("cart.urls")),
 ]
