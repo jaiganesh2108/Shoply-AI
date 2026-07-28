5f52a1930228e51264f1436f18506df80511bd73
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 13:43:06 +0530

    feat: initialize the user

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 from products.models import Product
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
