a05310561cc5d9941ea0b5e73b19a1cec4f9c2ff
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 18:52:20 +0530

    feat: created payment app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('api/', include('users.urls')),
     path('api/products/', include('products.urls')),
     path("api/cart/", include("cart.urls")),
+    path("api/orders/", include("orders.urls")),
 ]
