6f3169c36412a73ee5b39ae4c41d0c5e3b3f96a1
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 13:48:55 +0530

    feat: import the product key

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -6,3 +6,9 @@ from products.models import Product
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name}"
