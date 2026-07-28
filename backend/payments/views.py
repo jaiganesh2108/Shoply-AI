452bd83962f227649e97440717ef1f066957ce71
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 19:05:04 +0530

    feat: create urls for payments

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Payment
+from .serializers import PaymentSerializer
+
+
+class PaymentListCreateView(generics.ListCreateAPIView):
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Payment.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PaymentDetailView(generics.RetrieveAPIView):
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Payment.objects.filter(user=self.request.user)
