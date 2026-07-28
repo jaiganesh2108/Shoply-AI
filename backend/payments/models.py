82b26804d5fc80afd39883bb0dce8a614fa6bed5
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 19:00:29 +0530

    feat: create admin for the payment

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from orders.models import Order
+
+class Payment(models.Model):
+    PAYMENT_METHODS = [
+        ("COD", "Cash on Delivery"),
+        ("ONLINE", "Online Payment"),
+    ]
+
+    PAYMENT_STATUS = [
+        ("Pending", "Pending"),
+        ("Success", "Success"),
+        ("Failed", "Failed"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    order = models.OneToOneField(
+        Order,
+        on_delete=models.CASCADE
+    )
+
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    payment_method = models.CharField(
+        max_length=20,
+        choices=PAYMENT_METHODS
+    )
+
+    payment_status = models.CharField(
+        max_length=20,
+        choices=PAYMENT_STATUS,
+        default="Pending"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment #{self.id}"
