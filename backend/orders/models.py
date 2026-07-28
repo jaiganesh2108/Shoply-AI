dcba0b9321ab609ab394fd630a63be6f2c9e5a25
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 18:28:08 +0530

    feat: creating class and creating models of status choices

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Confirmed', 'Confirmed'),
+        ('Delivered', 'Delivered'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    
