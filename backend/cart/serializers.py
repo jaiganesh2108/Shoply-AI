66499611ce7686872afa5c73a3eff2644cb4878f
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 14:04:03 +0530

    feat: class serializer with model and fields

--- backend/cart/serializers.py
+++ backend/cart/serializers.py
@@ -2,4 +2,6 @@ from rest_framework import serializers
 from .models import Cart
 
 class CartSerializer(serializers.ModelSerializer):
-    
+    class Meta:
+        model = Cart
+        fields = "__all__"
