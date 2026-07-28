a6869326bb74016938fdca6fef9adf552b31261f
jaiganesh2108 <jaig7335@gmail.com>
2026-07-28 14:31:05 +0530

    feat: create views to the cart

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -12,3 +12,6 @@ class CartListCreateView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return Cart.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
