5d046747248ea6499386619755eee1094f3af4dc
jaiganesh2108 <jaig7335@gmail.com>
2026-07-29 21:30:28 +0530

    feat: adding the order items

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import User
+from products.models import Product
+
 
 class Order(models.Model):
     STATUS_CHOICES = [
@@ -20,3 +22,25 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    quantity = models.PositiveIntegerField(default=1)
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
