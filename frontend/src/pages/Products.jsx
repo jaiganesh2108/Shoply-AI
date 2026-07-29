f2fa378518411bf6a570eab5b3318061ddab2c0c
jaiganesh2108 <jaig7335@gmail.com>
2026-07-29 19:29:25 +0530

    feat: commit product api integration

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,14 +1,101 @@
-import { useEffect } from "react";
-import api from "../api/axios";
+import { useEffect, useState } from "react";
+import { getProducts } from "../api/products";
+
 
 function Products() {
-  useEffect(() => {
-    api.get("products/")
-      .then((res) => console.log(res.data))
-      .catch((err) => console.error(err));
-  }, []);
 
-  return <h2>Products Page</h2>;
+    const [products, setProducts] = useState([]);
+
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+
+        fetchProducts();
+
+    }, []);
+
+
+
+    const fetchProducts = async () => {
+
+        try {
+
+            const data = await getProducts();
+
+            setProducts(data);
+
+        }
+
+        catch(error){
+
+            console.log(error);
+
+        }
+
+        finally{
+
+            setLoading(false);
+
+        }
+
+    };
+
+
+
+    if(loading){
+
+        return <h2>Loading products...</h2>;
+
+    }
+
+
+
+    return (
+
+        <div>
+
+            <h1>
+                Products
+            </h1>
+
+
+            {
+                products.map((product)=>(
+
+                    <div key={product.id}>
+
+                        <h2>
+                            {product.name}
+                        </h2>
+
+
+                        <p>
+                            {product.description}
+                        </p>
+
+
+                        <h3>
+                            ₹ {product.price}
+                        </h3>
+
+
+                        <p>
+                            Stock: {product.stock}
+                        </p>
+
+
+                    </div>
+
+                ))
+            }
+
+
+        </div>
+
+    );
+
 }
+
 
 export default Products;
