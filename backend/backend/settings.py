824a389b4a2fd4c4dd8f71b7041a4dc45e13e448
jaiganesh2108 <jaig7335@gmail.com>
2026-09-24 21:13:16 +0530

    feat: home page updation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'shoply_db',
         "USER": "postgres",
-        "PASSWORD": "1234",   # Replace with your password
+        "PASSWORD": "jai@123",   # Replace with your password
         "HOST": "localhost",
         "PORT": "5432",
     }
